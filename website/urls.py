dc3507d36ceb75528979f3d56d98d20ef868f2b1
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-08-25 21:44:46 +0530

    one

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'website/landing_page.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
