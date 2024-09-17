27f6cd41afcd8e8bc434bc3421e4da62cbe1c443
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-09-17 12:23:08 +0530

    fix: missing FEFAULT_TO_EMAIL from settings.py

--- bds/settings.py
+++ bds/settings.py
@@ -146,6 +146,7 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_TO_EMAIL = os.getenv('DEFAULT_TO_EMAIL')
 
 
 # Logging settings
